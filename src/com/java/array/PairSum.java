20fab474bfd59c1d960d63809f356fdc02048b39
alokray92 <alokray2010@gmail.com>
2018-08-16 00:35:40 +0530

    commited code

--- src/com/java/array/PairSum.java
+++ src/com/java/array/PairSum.java
@@ -3,7 +3,7 @@ package com.java.array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+/*This is a comment*/
 public class PairSum {
 	public static void main(String[] args) {
 		int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
